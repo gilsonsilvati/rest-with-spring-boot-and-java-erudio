fc8a71e677d37559ae5c075deebdf2ae84abf1de
Gilson Silva <gilsonsilvati@gmail.com>
2022-07-12 16:34:00 -0300

    Migration with FlyWay

--- src/main/java/br/com/erudio/controllers/PersonController.java
+++ src/main/java/br/com/erudio/controllers/PersonController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 
     @Autowired
---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,3 @@
+INSERT INTO `person` VALUES
+    (1,'Uberlândia - Minas Gerais - Brazil','Leandro','Male','Costa'),
+    (2,'Smiljan - Croatia','Nikola','Male','Tesla');

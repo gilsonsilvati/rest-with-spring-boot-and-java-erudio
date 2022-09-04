697bbe1577cebfee557e15e861099ac77d69ac65
Gilson Silva <gilsonsilvati@gmail.com>
2022-09-04 16:04:49 -0300

    Creating exceptions customized. :construction:

--- src/main/java/br/com/erudio/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/br/com/erudio/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.erudio.exceptions.handler;
 
 import br.com.erudio.exceptions.ExceptionResponse;
+import br.com.erudio.exceptions.InvalidJwtAuthenticationException;
 import br.com.erudio.exceptions.RequiredObjectIsNullException;
 import br.com.erudio.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
+
         var exceptionResponse = buildExceptionResponse(ex.getMessage(), request.getDescription(false));
 
         return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -24,6 +26,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public final ResponseEntity<ExceptionResponse> handleNotFoundExceptions(ResourceNotFoundException ex, WebRequest request) {
+
         var exceptionResponse = buildExceptionResponse(ex.getMessage(), request.getDescription(false));
 
         return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
@@ -31,12 +34,22 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 
     @ExceptionHandler(RequiredObjectIsNullException.class)
     public final ResponseEntity<ExceptionResponse> handleBadRequestException(RequiredObjectIsNullException ex, WebRequest request) {
+
         var exceptionResponse = buildExceptionResponse(ex.getMessage(), request.getDescription(false));
 
         return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(InvalidJwtAuthenticationException.class)
+    public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationExceptions(InvalidJwtAuthenticationException ex, WebRequest request) {
+
+        var exceptionResponse = buildExceptionResponse(ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
+    }
+
     private ExceptionResponse buildExceptionResponse(String message, String description) {
+
         return new ExceptionResponse(ZonedDateTime.now(), message, description);
     }
 }
